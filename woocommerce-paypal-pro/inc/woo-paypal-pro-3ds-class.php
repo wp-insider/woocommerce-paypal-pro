588ebe1e3a0bc2147a5a098d655ba3e03113bfe3
erommel <eminemwalsh@gmail.com>
2019-04-18 13:27:28 +0300

    Revert "More work on 3D Secure support."
    This reverts commit 2e51be8fda91c663891773ae821410a1688e4696.

--- woocommerce-paypal-pro/inc/woo-paypal-pro-3ds-class.php
+++ woocommerce-paypal-pro/inc/woo-paypal-pro-3ds-class.php
@@ -6,9 +6,6 @@ class WC_Paypal_Pro_3DS {
     var $order_id	 = 0;
 
     function __construct( $wc_pp_pro_action = false ) {
-
-	require_once(WC_PP_PRO_ADDON_PATH . '/public/assets/views/3ds-tpl.php');
-
 	if ( $wc_pp_pro_action === 'iframe' ) {
 	    $this->three_d_secure_iframe();
 	}
@@ -16,7 +13,7 @@ class WC_Paypal_Pro_3DS {
 	    $this->three_d_secure_check();
 	}
 	if ( $wc_pp_pro_action === 'do_auth' ) {
-	    add_action( 'init', array( $this, 'three_d_secure_do_auth' ) );
+	    add_action( 'woocommerce_after_register_post_type', array( $this, 'three_d_secure_do_auth' ) );
 	}
     }
 
@@ -39,24 +36,6 @@ class WC_Paypal_Pro_3DS {
 	$this->set_order_id( $order_id );
     }
 
-    public static function get_centinel_lib() {
-	require_once(WC_PP_PRO_ADDON_PATH . '/lib/centinel/CentinelClient.php');
-	require_once(WC_PP_PRO_ADDON_PATH . '/lib/centinel/CentinelConfig.php');
-	require_once(WC_PP_PRO_ADDON_PATH . '/lib/centinel/CentinelUtility.php');
-
-	$payment_gateways = WC_Payment_Gateways::instance();
-
-	$gws = $payment_gateways->payment_gateways();
-
-	$gw = $gws[ 'paypalpro' ];
-	if ( ! defined( 'CENTINEL_MERCHANT_ID' ) ) {
-	    define( 'CENTINEL_MERCHANT_ID', $gw->settings[ '3ds_merch_id' ] );
-	    define( 'CENTINEL_PROCESSOR_ID', $gw->settings[ '3ds_proc_id' ] );
-	    define( 'CENTINEL_TRANSACTION_PWD', $gw->settings[ 'trans_pass' ] );
-	    define( 'CENTINEL_MAPS_URL', $gw->settings[ 'trans_url' ] );
-	}
-    }
-
     function three_d_secure_check() {
 
 	$this->get_order_id_from_get();
@@ -69,51 +48,223 @@ class WC_Paypal_Pro_3DS {
 	    'wc_pp_pro_action'	 => 'iframe',
 	    'wc_pp_pro_order'	 => $this->order_id,
 	), get_site_url( null, '/' ) );
-
-	$body	 = '';
-	ob_start();
 	?>
-	<div id="header">
-	    <h3>3D Secure Check</h3>
-	    <div id="description">For your security, please fill out the form below to complete your order.
-		Do not click the refresh or back button or this transaction may be interrupted or cancelled.
-	    </div>
-	    <div id="spinner">
-		<div class="spinner"><i></i><i></i><i></i><i></i></div>
-	    </div>
-	</div>
-	<iframe onload="content_finished_loading(this);" id="iframe" frameborder="0" height="100%" width="100%" src='<?php echo $iframe_url; ?>'>
-	    Frames are currently disabled or not supported by your browser. Please click <a href="<?php echo $iframe_url; ?>">here</a> to continue processing your transaction.
-	</iframe>
-	<script>
-	    var iframeChangeCount = 0;
-	    function content_finished_loading(iframe) {
-		try {
-		    var url = iframe.contentWindow.location.href;
-		    if (url === '<?php echo esc_js( $this->trans_data[ 'TermUrl' ] ); ?>') {
-			iframe.style.display = "none";
-			document.getElementById("description").style.display = "none";
-			document.getElementById("spinner").style.display = "block";
+	<!doctype html>
+	<html>
+	    <head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>3D Secure Check</title>
+		<link rel="stylesheet" type="text/css" href="<?php echo WC_PP_PRO_ADDON_URL; ?>/public/assets/css/pure-min.css">
+		<style>
+		    html, body {
+			height: 100%;
 		    }
-		} catch (error) {
-		    iframeChangeCount++;
-		    if (iframeChangeCount >= 2) {
-			document.getElementById("iframe").style.display = "none";
-			document.getElementById("description").style.display = "none";
-			document.getElementById("spinner").style.display = "block";
+		    body {
+			margin:0px;
+			padding:0px;
+			overflow:hidden;
 		    }
-		}
-	    }
-	</script>
+		    #iframe {
+			width: 100%;
+			height: 100%;
+			margin-top:20px;
+			padding: 5px;
+			overflow: scroll;
+		    }
+		    #header {
+			padding: 5px;
+			text-align: center;
+			background-color: #f0f0f0;
+			border-bottom: 1px solid #d0d0d0;
+		    }
+		    #description {
+			margin-bottom: 10px;
+		    }
+		    #spinner{
+			margin:0 auto;
+			width:97px;
+		    }
+		    #spinner {
+			display: none;
+			margin-bottom: 5px;
+		    }
+		    .spinner i{
+			width: 19px;
+			height: 19px;
+			display: inline-block;
+			border-radius: 50%;
+			background: rgb(17,91,219);
+		    }
+		    .spinner i:first-child{
+			opacity: 0;
+			animation:spinner-ani2 0.58s linear infinite;
+			-o-animation:spinner-ani2 0.58s linear infinite;
+			-ms-animation:spinner-ani2 0.58s linear infinite;
+			-webkit-animation:spinner-ani2 0.58s linear infinite;
+			-moz-animation:spinner-ani2 0.58s linear infinite;
+			transform:translate(-19px);
+			-o-transform:translate(-19px);
+			-ms-transform:translate(-19px);
+			-webkit-transform:translate(-19px);
+			-moz-transform:translate(-19px);
+		    }
+		    .spinner i:nth-child(2),
+		    .spinner i:nth-child(3){
+			animation:spinner-ani3 0.58s linear infinite;
+			-o-animation:spinner-ani3 0.58s linear infinite;
+			-ms-animation:spinner-ani3 0.58s linear infinite;
+			-webkit-animation:spinner-ani3 0.58s linear infinite;
+			-moz-animation:spinner-ani3 0.58s linear infinite;
+		    }
+		    .spinner i:last-child{
+			animation:spinner-ani1 0.58s linear infinite;
+			-o-animation:spinner-ani1 0.58s linear infinite;
+			-ms-animation:spinner-ani1 0.58s linear infinite;
+			-webkit-animation:spinner-ani1 0.58s linear infinite;
+			-moz-animation:spinner-ani1 0.58s linear infinite;
+		    }
+
+		    @keyframes spinner-ani1{
+			100%{
+			    transform:translate(39px);
+			    opacity: 0;
+			}
+		    }
+
+		    @-o-keyframes spinner-ani1{
+			100%{
+			    -o-transform:translate(39px);
+			    opacity: 0;
+			}
+		    }
+
+		    @-ms-keyframes spinner-ani1{
+			100%{
+			    -ms-transform:translate(39px);
+			    opacity: 0;
+			}
+		    }
+
+		    @-webkit-keyframes spinner-ani1{
+			100%{
+			    -webkit-transform:translate(39px);
+			    opacity: 0;
+			}
+		    }
+
+		    @-moz-keyframes spinner-ani1{
+			100%{
+			    -moz-transform:translate(39px);
+			    opacity: 0;
+			}
+		    }
+
+		    @keyframes spinner-ani2{
+			100%{
+			    transform:translate(19px);
+			    opacity: 1;
+			}
+		    }
+
+		    @-o-keyframes spinner-ani2{
+			100%{
+			    -o-transform:translate(19px);
+			    opacity: 1;
+			}
+		    }
+
+		    @-ms-keyframes spinner-ani2{
+			100%{
+			    -ms-transform:translate(19px);
+			    opacity: 1;
+			}
+		    }
+
+		    @-webkit-keyframes spinner-ani2{
+			100%{
+			    -webkit-transform:translate(19px);
+			    opacity: 1;
+			}
+		    }
+
+		    @-moz-keyframes spinner-ani2{
+			100%{
+			    -moz-transform:translate(19px);
+			    opacity: 1;
+			}
+		    }
+
+		    @keyframes spinner-ani3{
+			100%{
+			    transform:translate(19px);
+			}
+		    }
+
+		    @-o-keyframes spinner-ani3{
+			100%{
+			    -o-transform:translate(19px);
+			}
+		    }
+
+		    @-ms-keyframes spinner-ani3{
+			100%{
+			    -ms-transform:translate(19px);
+			}
+		    }
+
+		    @-webkit-keyframes spinner-ani3{
+			100%{
+			    -webkit-transform:translate(19px);
+			}
+		    }
+
+		    @-moz-keyframes spinner-ani3{
+			100%{
+			    -moz-transform:translate(19px);
+			}
+		    }
+		</style>
+		<style>
+		</style>
+	    </head>
+	    <body>
+		<div id="header">
+		    <h3>3D Secure Check</h3>
+		    <div id="description">For your security, please fill out the form below to complete your order.
+			Do not click the refresh or back button or this transaction may be interrupted or cancelled.
+		    </div>
+		    <div id="spinner">
+			<div class="spinner"><i></i><i></i><i></i><i></i></div>
+		    </div>
+		</div>
+		<iframe onload="content_finished_loading(this);" id="iframe" frameborder="0" height="100%" width="100%" src='<?php echo $iframe_url; ?>'>
+		    Frames are currently disabled or not supported by your browser.  Please click <a href="<?php echo $iframe_url; ?>">here</a> to continue processing your transaction.
+		</iframe>
+
+		<script>
+		    var iframeChangeCount = 0;
+		    function content_finished_loading(iframe) {
+			try {
+			    var url = iframe.contentWindow.location.href;
+			    if (url === '<?php echo esc_js( $this->trans_data[ 'TermUrl' ] ); ?>') {
+				iframe.style.display = "none";
+				document.getElementById("description").style.display = "none";
+				document.getElementById("spinner").style.display = "block";
+			    }
+			} catch (error) {
+			    iframeChangeCount++;
+			    if (iframeChangeCount >= 2) {
+				document.getElementById("iframe").style.display = "none";
+				document.getElementById("description").style.display = "none";
+				document.getElementById("spinner").style.display = "block";
+			    }
+			}
+		    }
+		</script>
+	    </body>
+	</html>
 	<?php
-	$body	 = ob_get_clean();
-
-	$args = array(
-	    'title'	 => "3D Secure Check",
-	    'body'	 => $body,
-	);
-
-	new WC_Paypal_Pro_3DS_tpl( $args );
 	exit;
     }
 
@@ -130,38 +281,38 @@ class WC_Paypal_Pro_3DS {
 	if ( empty( $this->trans_data ) ) {
 	    wp_die( 'No data found' );
 	}
-
-	$body	 = '';
-	ob_start();
 	?>
-	<form name="wpPPProLaunchACS" method="post" action="<?php echo $this->trans_data[ 'url' ]; ?>">
-	    <input type=hidden name="PaReq" value="<?php echo $this->trans_data[ 'PaReq' ]; ?>">
-	    <input type=hidden name="TermUrl" value="<?php echo $this->trans_data[ 'TermUrl' ]; ?>">
-	    <input type=hidden name="MD" value="<?php echo $this->trans_data[ 'MD' ]; ?>">
-	    <noscript>
-	    <center>
-		<font color="red">
-		<h2>Processing your Payer Authentication Transaction</h2>
-		<h3>JavaScript is currently disabled or is not supported by your browser.<br></h3>
-		<h4>Please click Submit to continue the processing of your transaction.</h4>
-		</font>
-		<input type="submit" value="Submit">
-	    </center>
-	    </noscript>
-	</form>
-	<script>
-	    document.body.onload = function () {
-		document.wpPPProLaunchACS.submit();
-	    }
-	</script>
+	<!doctype html>
+	<html>
+	    <head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Redirecting...</title>
+		<script language="javascript">
+		    function onLoadHandler() {
+			document.wpPPProLaunchACS.submit();
+		    }
+		</script>
+	    </head>
+	    <body onLoad="onLoadHandler();">
+		<form name="wpPPProLaunchACS" method="post" action="<?php echo $this->trans_data[ 'url' ]; ?>">
+		    <input type=hidden name="PaReq" value="<?php echo $this->trans_data[ 'PaReq' ]; ?>">
+		    <input type=hidden name="TermUrl" value="<?php echo $this->trans_data[ 'TermUrl' ]; ?>">
+		    <input type=hidden name="MD" value="<?php echo $this->trans_data[ 'MD' ]; ?>">
+		    <noscript>
+		    <center>
+			<font color="red">
+			<h2>Processing your Payer Authentication Transaction</h2>
+			<h3>JavaScript is currently disabled or is not supported by your browser.<br></h3>
+			<h4>Please click Submit to continue the processing of your transaction.</h4>
+			</font>
+			<input type="submit" value="Submit">
+		    </center>
+		    </noscript>
+		</form>
+	    </body>
+	</html>
 	<?php
-	$body	 = ob_get_clean();
-
-	$args = array(
-	    'title'	 => "Redirecting...",
-	    'body'	 => $body,
-	);
-	new WC_Paypal_Pro_3DS_tpl( $args );
 	exit;
     }
 
@@ -172,8 +323,9 @@ class WC_Paypal_Pro_3DS {
 	//TODO: add tests for empty values
 
 	$this->get_trans_data();
-
-	self::get_centinel_lib();
+	require_once(WC_PP_PRO_ADDON_PATH . '/lib/centinel/CentinelClient.php');
+	require_once(WC_PP_PRO_ADDON_PATH . '/lib/centinel/CentinelConfig.php');
+	require_once(WC_PP_PRO_ADDON_PATH . '/lib/centinel/CentinelUtility.php');
 
 	$centinelClient = new CentinelClient;
 
@@ -205,13 +357,13 @@ class WC_Paypal_Pro_3DS {
 	if ( empty( $PaResStatus ) || empty( $sign_verify ) ) {
 	    $errMsg = "ERROR: " . $errorDesc;
 	} else if ( (strcasecmp( 'Y', $PaResStatus ) == 0 || strcasecmp( 'A', $PaResStatus ) == 0) && (strcasecmp( 'Y', $sign_verify ) == 0) && (strcasecmp( '0', $errorNo ) == 0 || strcasecmp( '1140', $errorNo ) == 0) ) {
-	    // Transaction completed successfully.
+	    // Transaction completed successfully. 
 	    //$errMsg = "Transaction completed successfully. (ErrorNo: [{$errorNo}], ErrorDesc: [{$errorDesc}])";
 	} else if ( (strcasecmp( 'N', $PaResStatus ) == 0) && (strcasecmp( 'Y', $sign_verify ) == 0) && (strcasecmp( '0', $errorNo ) == 0 || strcasecmp( '1140', $errorNo ) == 0) ) {
-	    // Unable to authenticate. Provide another form of payment.
+	    // Unable to authenticate. Provide another form of payment. 
 	    $errMsg = "Unable to authenticate. Provide another form of payment. (ErrorNo: [{$errorNo}], ErrorDesc: [{$errorDesc}])";
 	} else {
-	    // Transaction complete however is pending review. Order will be shipped once payment is verified.
+	    // Transaction complete however is pending review. Order will be shipped once payment is verified. 
 //	    $errMsg		 = "Transaction complete however is pending review. Order will be shipped once payment is verified. (ErrorNo: [{$errorNo}], ErrorDesc: [{$errorDesc}])";
 //	    $redirectPage	 = 'ccResults.php';
 	} // end processing logic
@@ -224,19 +376,20 @@ class WC_Paypal_Pro_3DS {
 	    //auth successful
 	    $_POST	 = $this->trans_data[ "_POST" ];
 	    //set additional fields for PP transaction
+//	    $order		 = wc_get_order( $this->order_id );
+//	    $order_id	 = $order->get_id();
+	    $ppgw	 = new WC_PP_PRO_Gateway();
 	    $cavv	 = $centinelClient->getValue( "Cavv" );
 	    $eciflag = $centinelClient->getValue( "EciFlag" );
 	    $xid	 = $centinelClient->getValue( "Xid" );
 	    $fields	 = array(
 		'VERSION'	 => '59.0',
 		'AUTHSTATUS3DS'	 => $PaResStatus,
-		'MPIVENDOR3DS'	 => $this->trans_data[ 'Enrolled' ],
+		'MPIVENDOR3DS'	 => $this->trans_data[ 'enrolled' ],
 		'CAVV'		 => $cavv,
 		'ECI3DS'	 => $eciflag,
 		'XID'		 => $xid,
 	    );
-	    //create gateway and process payment
-	    $ppgw	 = new WC_PP_PRO_Gateway();
 	    $ppgw->set_additional_paypal_req_fields( $fields );
 	    $res	 = $ppgw->process_payment( $this->order_id, true );
 	    if ( isset( $res[ 'redirect' ] ) ) {
@@ -247,23 +400,24 @@ class WC_Paypal_Pro_3DS {
 	    wc_add_notice( $errMsg, 'error' );
 	}
 	ob_end_clean();
-
-	$body	 = '';
-	ob_start();
 	?>
-	<script>
-	    (function () {
-		window.top.location = '<?php echo $redir_url; ?>';
-	    })();
-	</script>
+	<!doctype html>
+	<html>
+	    <head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>3D Secure Check</title>
+		<script>
+		    (function () {
+			window.top.location = '<?php echo $redir_url; ?>';
+		    })();
+		</script>
+	    </head>
+	    <body>
+		Performing 3D Secure check...
+	    </body>
+	</html>
 	<?php
-	$body	 = ob_get_clean();
-
-	$args = array(
-	    'title'	 => "Redirecting...",
-	    'body'	 => $body,
-	);
-	new WC_Paypal_Pro_3DS_tpl( $args );
 	exit;
     }
 
